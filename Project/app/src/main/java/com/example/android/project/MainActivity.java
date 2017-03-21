43f06d25e62adf2323520aea10e3a5f546d53609
Jobonno <jobonno@gmail.com>
2017-03-21 11:49:40 +1100

    DEV-1 Added comment to MainActivity

--- Project/app/src/main/java/com/example/android/project/MainActivity.java
+++ Project/app/src/main/java/com/example/android/project/MainActivity.java
@@ -35,6 +35,7 @@ public String msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Inflate the view
         setContentView(R.layout.activity_main);
     }
 
